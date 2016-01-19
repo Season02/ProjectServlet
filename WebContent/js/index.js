735b016dd13165ea0a444b2c7434828c081ca163
Season02 <sayhanabi@hotmail.com>
2016-01-19 11:11:01 +0800

    finally hidde headline worked!

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,16 +1,30 @@
-$(function(){	
+$(function()
+{	
     var cubuk_seviye = $(document).scrollTop();
     var header_yuksekligi = $('.headline').outerHeight();
 
-    $(window).scroll(function() {
+    $(window).scroll(function() 
+    {
         var kaydirma_cubugu = $(document).scrollTop();
 
-        if (kaydirma_cubugu > header_yuksekligi){$('.headline').addClass('g');} 
-        else {$('.headline').removeClass('g');}
+        if (kaydirma_cubugu > header_yuksekligi)
+        {
+        	$('.headline').addClass('g');
+        } 
+        else 
+        {
+        	$('.headline').removeClass('g');
+        }
 
-        if (kaydirma_cubugu > cubuk_seviye){$('.headline').removeClass('s');} 
-        else {$('.headline').addClass('s');}				
+        if (kaydirma_cubugu > cubuk_seviye)
+        {
+        	$('.headline').removeClass('s');
+        } 
+        else 
+        {
+        	$('.headline').addClass('s');
+        }				
 
         cubuk_seviye = $(document).scrollTop();	
-     });
+    });
 });
