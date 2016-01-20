61b5be335386c065e5a36bad675951bc34c5c392
Season02 <sayhanabi@hotmail.com>
2016-01-20 15:30:17 +0800

    authentication

--- src/com/sayhanabi/servlets/LoginServlet.java
+++ src/com/sayhanabi/servlets/LoginServlet.java
@@ -1,11 +1,15 @@
 package com.sayhanabi.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sayhanabi.factory.DaoFactory;
 
 /**
  * Servlet implementation class LoginServlet
@@ -35,27 +39,41 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		response.setContentType("text/html;charset=utf-8") ;
+		request.setCharacterEncoding("utf-8") ;
 		String username = request.getParameter("username");     
-        String password = request.getParameter("password");        
-          
-        //服务器端打印信息  
-        //System.out.println("username=" + username);  
-        //System.out.println("password=" + password);  
-        //设置编码格式  
-        response.setContentType("text/html;charset=GB2312");
+        String password = request.getParameter("password");  
+        HttpSession session =  request.getSession();
+        int id = -1;
+        System.out.println(username);
+        System.out.println(password);
         
-        //返回html页面  
-        response.getWriter().println("<html>");  
-        response.getWriter().println("<head>");     
-        response.getWriter().println("<title>登录信息</title>");      
-        response.getWriter().println("</head>");
-        response.getWriter().println("<body>");
-        if(username.equals("Season02") && password.equals("651234"))
-        	response.getWriter().println("Welcome [" + username + "] user login successful!!!");  
+        if(( id = DaoFactory.getManager().authenticationManager(username, password) ) > -1)
+        {
+        	request.setAttribute("id",id);
+            response.sendRedirect("success.jsp") ;
+        }
         else
-        	response.getWriter().println("err!!!");
-        response.getWriter().println("</body>");    
-        response.getWriter().println("</html>");
+        {
+            session.setAttribute("message", "用户名或密码不匹配。");
+            response.sendRedirect("fail.jsp") ;
+        }
+
+//        //设置编码格式  
+//        response.setContentType("text/html;charset=GB2312");
+//        
+//        //返回html页面  
+//        response.getWriter().println("<html>");  
+//        response.getWriter().println("<head>");     
+//        response.getWriter().println("<title>登录信息</title>");      
+//        response.getWriter().println("</head>");
+//        response.getWriter().println("<body>");
+//        if(username.equals("Season02") && password.equals("651234"))
+//        	response.getWriter().println("Welcome [" + username + "] user login successful!!!");  
+//        else
+//        	response.getWriter().println("err!!!");
+//        response.getWriter().println("</body>");    
+//        response.getWriter().println("</html>");
 	}
 
 }
